9cef17ee0beb483376587fd5f4b573330e2fe922
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-03-30 18:38:00 -0400

    Applies new pyomo logic to CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,5 +4,7 @@ from batteryopt.cli import batteryopt
 class TestCli:
     def test_batteryopt(self):
         runner = CliRunner()
-        result = runner.invoke(batteryopt, ["output.csv"])
+        result = runner.invoke(batteryopt, ["../data/demand_aggregated.csv",
+                                            "../data/PV_generation_aggregated.csv",
+                                            "output.csv"])
         assert result.exit_code == 0
