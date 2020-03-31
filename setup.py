138882eafca7d3e94f67046865eb9c6fbbf22ecf
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-03-31 10:05:34 -0400

    Setup.py uses requirements.txt file

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
+import codecs
+import os
+from os import path
+
 from setuptools import setup, find_packages
+
+here = os.getcwd()
+# Get the long description from the README file
+with codecs.open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+with open(path.join(here, "requirements.txt")) as f:
+    requirements_lines = f.readlines()
+install_requires = [r.strip() for r in requirements_lines]
 
 setup(
     name="batteryopt",
     version="0.1.0",
     packages=find_packages(),
-    url="",
+    url="https://github.com/MITSustainableDesignLab/batteryopt",
     license="MIT",
     author="Jakub Tomasz Szczesniak",
     author_email="jakubszc@mit.edu",
-    description="",
-    install_requires=["Click", 'pandas'],
+    description="battery operation optimization",
+    long_description=long_description,
+    install_requires=install_requires,
     entry_points="""
         [console_scripts]
         batteryopt=batteryopt.cli:batteryopt
