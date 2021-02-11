3aa3d8938d21d5105ca179cd4eae9ceb2b3bfd62
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-02-11 10:52:57 -0500

    add classifiers

--- setup.py
+++ setup.py
@@ -22,10 +22,27 @@ setup(
     author_email="jakubszc@mit.edu",
     description="battery operation optimization",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     install_requires=install_requires,
     entry_points="""
         [console_scripts]
         batteryopt=batteryopt.cli:batteryopt
     """,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Researchers",
+        "Topic :: Optimization :: Battery",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
